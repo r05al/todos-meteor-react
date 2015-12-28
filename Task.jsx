ecfdd5c13a1b162e1bbf60795b7081fec5963032
Peter Rosal <peter.rosal@gmail.com>
2015-12-28 13:13:36 -0600

    Add checked and delete function and render delete button and checkbox

--- Task.jsx
+++ Task.jsx
@@ -2,9 +2,36 @@ Task = React.createClass({
   propTypes: {
     task: React.PropTypes.object.isRequired
   },
+
+  toggleChecked() {
+    // set the checked property to opposite of current value
+    Tasks.update(this.props.task._id, {
+      $set: {checked: ! this.props.task.checked}
+    });
+  },
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  },
+
   render() {
+    // give className depending on checked status for CSS styling
+    const taskClassName = this.props.task.checked ? "checked" : "";
+
     return(
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly={true}
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked} />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 });
