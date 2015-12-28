72d080c5d624aa5cc316bdfbd8fce2ed4a79b6d5
Peter Rosal <peter.rosal@gmail.com>
2015-12-28 12:59:16 -0600

    Add form to render and handleSubmit function

--- App.jsx
+++ App.jsx
@@ -1,16 +1,39 @@
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
-    ];
+
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      tasks: Tasks.find({}).fetch()
+    }
   },
 
+  // getTasks() {
+  //   return [
+  //     { _id: 1, text: "This is task 1" },
+  //     { _id: 2, text: "This is task 2" },
+  //     { _id: 3, text: "This is task 3" }
+  //   ];
+  // },
+
   renderTasks() {
-    return this.getTasks().map((task) => {
+    return this.data.tasks.map((task) => {
       return <Task key={task._id} task={task} />;
     });
+  },
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    var text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() //current time
+    });
+
+    // clears form
+    ReactDOM.findDOMNode(this.refs.textInput).value="";
   },
 
   render() {
@@ -18,6 +41,13 @@ App = React.createClass({
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks" />
+          </form>
         </header>
 
         <ul>
