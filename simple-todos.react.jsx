a9106335f5e1af6ee4e4f8aa694681a33e53e01b
Peter Rosal <peter.rosal@gmail.com>
2015-12-28 17:24:35 -0500

    Add subscribe and publish methods for tasks

--- simple-todos.react.jsx
+++ simple-todos.react.jsx
@@ -5,7 +5,54 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_ONLY"
   });
 
+  Meteor.subscribe("tasks");
+
   Meteor.startup(function(){
     ReactDOM.render(<App />, document.getElementById("render-target"));
   });
 }
+
+if (Meteor.isServer) {
+  Meteor.publish("tasks", function() {
+    return Tasks.find();
+  });
+}
+
+Meteor.methods({
+  addTask(text) {
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+
+  removeTask(taskId) {
+    const task = Tasks.findOne(taskId);
+
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.remove(taskId);
+  },
+
+  setChecked(taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+
+  setPrivate(taskId, setToPrivate) {
+    const task = Tasks.findOne(taskId);
+
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+  }
+});
